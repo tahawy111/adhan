2000e4901288dd08fcc752fd4e7d83eae6eab2d1
TAHAWY <amertahawy111@gmail.com>
2023-02-14 01:15:55 +0200

    remove react helmet from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
+  <App />
 );
